c6296fade9ba33dce7882d7bfd1efe2a4b492505
himanshunegi89 <86564389+himanshunegi89@users.noreply.github.com>
2024-05-10 00:20:10 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Red Love, U&Me";
+        return "Red Love, U & Me";
     }
 
 }
